90e6b507ff52138d91f590f59d92751e125d2b86
Igor Pereiaslovsckiy <Pereiaslovckiy.Igor@hotmail.com>
2019-03-19 00:51:46 +0300

    18.03.2018 night

--- LibOnline/Controllers/Books/BooksCategoriesController.cs
+++ LibOnline/Controllers/Books/BooksCategoriesController.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using LibOnline.Models;
+using LibOnline.Models.BooksCategories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibOnline.Controllers
 {
     public class BooksCategoriesController : Controller
     {
-        public IActionResult BooksCategories(int idCategory)
+        public IActionResult BooksCategories()
         {
             return View();
         }
+
+
+        public IActionResult GetBooksCategories(int idCategory, int pageNumber)
+        {
+            List<BooksCatogoriesToShow> books = new List<BooksCatogoriesToShow>();
+            List<BooksCategories> categories = new List<BooksCategories>();
+
+            SqlParameter CategoryId = new SqlParameter("@IdCategory", idCategory);
+            SqlParameter pNumber = new SqlParameter("@PageNumber", pageNumber);
+           
+            using (ApplicationContext db = new ApplicationContext())
+                categories = db.booksCategories.FromSql($"EXECUTE [books].[GetBooksCategories] {CategoryId}, {pNumber}").ToList();
+
+            categories.ForEach(item => books.Add(new BooksCatogoriesToShow(item)));
+            ViewBag.booksCategories = books;
+
+            return View("BooksCategories");
+        }//GetBooksCategories
     }
 }
--- LibOnline/Models/ApplicationContext.cs
+++ LibOnline/Models/ApplicationContext.cs
@@ -23,25 +23,6 @@ namespace LibOnline.Models
             Database.EnsureCreated();
         }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    string adminRoleName = "admin";
-        //    string userRoleName = "user";
-
-        //    string adminEmail = "admin@mail.ru";
-        //    string adminLogin = "Avalod";
-        //    string adminPassword = "123456";
-
-        //    // добавляем роли
-        //    Role adminRole = new Role { Id = 1, Name = adminRoleName };
-        //    Role userRole = new Role { Id = 2, Name = userRoleName };
-        //    User adminUser = new User { Id = 1, Login = adminLogin, Email = adminEmail, Password = adminPassword, RoleId = adminRole.Id };
-
-        //    modelBuilder.Entity<Role>().HasData(new Role[] { adminRole, userRole });
-        //    modelBuilder.Entity<User>().HasData(new User[] { adminUser });
-        //    base.OnModelCreating(modelBuilder);
-        //}
-
 
         public ApplicationContext()
         {

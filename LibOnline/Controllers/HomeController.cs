916b6c2cc1bc45e672c2d75314b30a98e9e714dd
pereiaslovsckiy.igor <pereiaslovsckiy.igor@hotmail.com>
2019-03-21 00:27:14 +0300

    Home Page

--- LibOnline/Controllers/HomeController.cs
+++ LibOnline/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LibOnline.Models;
 using LibOnline.Models.Categories;
 using Microsoft.EntityFrameworkCore;
 using LibOnline.Models.Books;
-using System.Data.SqlClient;
 
 namespace LibOnline.Controllers
 {
